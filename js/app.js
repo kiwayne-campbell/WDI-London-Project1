fae1bf742c7d59c6d5be8bbf3f911397c62d1170
Kiwayne-Campbell <kiwayne.campbell@gmail.com>
2016-10-08 14:53:56 +0100

    working code but struggling on array being returned rather than data in array

--- js/app.js
+++ js/app.js
@@ -3,24 +3,39 @@ console.log("alive");
 var gameScore = 0;
 var scoreUpdate = document.getElementById('score');
 var jakeChoices = ["test1", "test2", "test3", "test4", "test5"];
-var randomise = Math.floor(Math.random() * jakeChoices.length);
-var typeChoices = document.getElementsByClassName('happyChoice').innerHTML=randomise;
 
  var start = document.getElementById('button').addEventListener('click', function() {
 
   console.log("button working");
 
+  var randomise = Math.floor(Math.random() * jakeChoices.length);
+  var typeChoices = document.getElementById('happy').innerHTML=randomise;
+
+
   document.addEventListener('keyup', function(e)  {
 
     if (e.key === jakeChoices) {
 
-      console.log ("correct word entered");
+      console.log ("works");
+      gameScore +=10;
     }
-     gameScore +=10;
 
      // set time out function to deduct score here? so if jakeChoices not entered quick enough cycles to next word to enter?
 
+   else {
+
+    console.log("deduct score");
+    gameScore -= 1;
+  }
+
   });
+
+
+
+//
+
+
+scoreUpdate.innerHTML = gameScore;
 
 
 });
