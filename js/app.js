1855e4b00ad2a9778c5455abbb739bfea22903d6
Kiwayne-Campbell <kiwayne.campbell@gmail.com>
2016-10-08 21:30:50 +0100

    managed to get choices to refesh and slice

--- js/app.js
+++ js/app.js
@@ -6,66 +6,78 @@ var gameScore = 0;
 var jakeChoices = ["test1", "test2", "test3", "test4", "test5"];
 var start = document.getElementById('button');
 var happyChoice = document.getElementById('happyChoice');
+var imageUpdate = document.getElementById('jakeImage');
 
-scoreUpdate.textContent = gameScore;
 
 start.addEventListener('click', function() {
 
   console.log("button working");
+
+  scoreUpdate.textContent = gameScore;
 
   var choice = jakeChoices[Math.floor(Math.random() * jakeChoices.length)];
 
   happyChoice.textContent = choice;
 
 
-  // setTimeout(function(){ console.log(typeChoices); }, 3000);
-
   function refreshData() {
 
-       x = 5;
+  x = 3;
 
-console.log("works but not repeating JS");
-
+console.log("works");
 var choice = jakeChoices[Math.floor(Math.random() * jakeChoices.length)];
-
 happyChoice.textContent = choice;
 
     setTimeout(refreshData, x*1000);
+
+    document.addEventListener('keyup', function(e)  {
+
+      if (e.key === choice[0]) {
+
+        console.log ("works");
+        choice = choice.slice(1);
+        happyChoice.textContent = choice;
+        gameScore += 10;
+      }
+
+     else {
+
+      console.log("deduct score");
+      gameScore -= 15;
+    }
+
+    });
 }
 
 refreshData();
 
-
-//
-// window.setInterval(function(){
-//
-//
-//
-// }, 5000);
-
-
-  document.addEventListener('keyup', function(e)  {
-
-    if (e.key === choice[0]) {
-
-      console.log ("works");
-      choice = choice.slice(1);
-      happyChoice.textContent = choice;
-      gameScore += 10;
-    }
-
-     // set time out function to deduct score here? so if jakeChoices not entered quick enough cycles to next word to enter?
-
-   else {
-
-    console.log("deduct score");
-    gameScore -= 15;
-  }
-
-  });
-
+  // document.addEventListener('keyup', function(e)  {
+  //
+  //   if (e.key === choice[0]) {
+  //
+  //     console.log ("works");
+  //     choice = choice.slice(1);
+  //     happyChoice.textContent = choice;
+  //     gameScore += 10;
+  //   }
+  //
+  //  else {
+  //
+  //   console.log("deduct score");
+  //   gameScore -= 15;
+  // }
+  //
+  // });
 
 scoreUpdate.textContent = gameScore;
 
+// this function evokes the change of image depending on gameScore
+
+// if (gameScore > 50) {
+//
+// document.getElementById()
+//
+//
+// }
 
 });
