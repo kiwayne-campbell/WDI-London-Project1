9c775bc7309c6ce8499ded530dbf6e03824c312c
Kiwayne-Campbell <kiwayne.campbell@gmail.com>
2016-10-09 10:20:08 +0100

    added timer function

--- js/app.js
+++ js/app.js
@@ -53,6 +53,36 @@ refreshData();
 
 scoreUpdate.innerHTML = gameScore;
 
+function countdown( elementName, minutes, seconds ) {
+    var element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer() {
+        msLeft = endTime - (+new Date());
+        if ( msLeft < 1000 ) {
+            element.innerHTML = "Times Up!";
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+
+    element = document.getElementById("timer");
+    endTime = (+new Date()) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
+
+countdown("timer", 1, 5 );
+
+
+
 // this function evokes the change of image depending on gameScore
 
 // if (gameScore > 50) {
