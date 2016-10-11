369f7cd4fd0043c574b67a4e5bf398fd295453fe
Kiwayne-Campbell <kiwayne.campbell@gmail.com>
2016-10-11 15:29:08 +0100

    sounds now working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-var jakeChoices = ["pancakes", "lady rainicorn", "sandwich", "beautopia", "pie", "adventure", "finn", "coffee", "burgers", "viola", "magical", "ice cream", "burrito", "lobster soul"];
+var jakeChoices = ["pancakes", "lady rainicorn", "time sandwich", "beautopia", "pie", "adventure", "finn", "coffee", "burgers", "viola", "magical", "ice cream", "burrito", "lobster soul", "korean food"];
 var start = document.getElementById('start');
 var timerDisplay = document.getElementById('timer');
 var happyChoice = document.getElementById('happyChoice');
@@ -7,21 +7,23 @@ var imageUpdate = document.getElementById('jakeImage');
 var imageUpdate2 = document.getElementById('finnImage');
 var scoreUpdate = document.getElementById('score');
 var replaceBoard = document.getElementById('resultDisplay');
-// var sounds = document.getElementById('adventureSounds');
+var adventureSounds = document.getElementById('adventureSounds');
 var resetGame = document.getElementById('restart');
 var gameScore = 0;
+var isPlaying = false;
 
 // add function to dcrease score - not sure if corrcet place but works for now
 var decrease = setInterval(function () {
-    gameScore -= 10;
-    scoreUpdate.innerHTML = gameScore;
-}, 1000);
+  gameScore -= 10;
+  scoreUpdate.innerHTML = gameScore;
+}, 1400);
 
 
 
 // start game on click
 
 start.addEventListener('click', function() {
+  isPlaying = true;
   var choice = jakeChoices[Math.floor(Math.random() * jakeChoices.length)];
   happyChoice.textContent = choice;
   happyChoice.style.fontSize = "60px";
@@ -37,23 +39,23 @@ start.addEventListener('click', function() {
 
   document.addEventListener('keyup', function(e)  {
 
-    if (e.key === choice[0]) {
+    if(!isPlaying) return false;
+
+    if (String.fromCharCode(e.keyCode).toLowerCase() === choice[0]) {
       console.log("works");
       choice = choice.slice(1);
       happyChoice.textContent = choice;
       gameScore += 5;
       scoreUpdate.innerHTML = gameScore;
-      // sounds.src = "https://upload.wikimedia.org/wikipedia/commons/e/ef/Eastern_Whipbird.ogg";
-      // sounds.play();
     }
     else {
-      gameScore -= 100;
+      gameScore -= 50;
       scoreUpdate.innerHTML = gameScore;
     }
 
 // make image update with new image once score goes above certain level
 
-    if(gameScore > 50) {
+    if(gameScore > 100) {
       imageUpdate.style.backgroundImage = "url(/images/happyjake2.gif)";
       imageUpdate2.style.backgroundImage = "url(/images/happyfinn.gif)";
     }
@@ -63,10 +65,12 @@ start.addEventListener('click', function() {
     }
 
     if(choice.length === 0) {
+      adventureSounds.play();
+
       choice = jakeChoices[Math.floor(Math.random() * jakeChoices.length)];
       happyChoice.textContent = choice;
-      }
-    });
+    }
+  });
 
 
   // my countdown timer - make less complex?
@@ -82,39 +86,33 @@ start.addEventListener('click', function() {
       msLeft = endTime - (+new Date());
 
 // add game win function here
-
-      if ( msLeft < 0 && gameScore > 50 ) {
-        console.log("grabbing win image");
+      if(msLeft < 0) {
         happyChoice.style.display = "none";
         replaceBoard.style.display = "block";
-        replaceBoard.style.backgroundImage = "url(/images/jake_the_dog___pure_css_adventure_time_wallpaper_by_sangreprimitiva-d5vs51f.png)";
-        replaceBoard.innerHTML = "Finn made Jake Happy! You win! Yaaaay!";
         clearInterval(decrease);
-        }
+        isPlaying = false;
 
-        else if ( msLeft < 0 && gameScore < 50 ) {
-          console.log("grabbing losing image");
-          happyChoice.style.display = "none";
-          replaceBoard.style.display = "block";
+        if(gameScore > 50) {
+          replaceBoard.style.backgroundImage = "url(/images/jake_the_dog___pure_css_adventure_time_wallpaper_by_sangreprimitiva-d5vs51f.png)";
+          replaceBoard.innerHTML = "Finn made Jake Happy! You win! Yaaaay!";
+        } else {
           replaceBoard.style.backgroundImage = "url(/images/Inspirational+wallpaper_f2b046_5473871.jpg)";
-          replaceBoard.innerHTML = "Jake is still sad! Finn lost! Nooo!";
-          clearInterval(decrease);
+          replaceBoard.innerHTML = "Jake is still sad! Noooo!";
         }
-
-       else {
-          time = new Date( msLeft );
-          hours = time.getUTCHours();
-          mins = time.getUTCMinutes();
-          element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
-          setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
-          }
-        }
-
-      element = document.getElementById(elementName);
-      endTime = (+new Date()) + 1000 * (60*minutes + seconds) + 500;
-      updateTimer();
+      } else {
+        time = new Date( msLeft );
+        hours = time.getUTCHours();
+        mins = time.getUTCMinutes();
+        element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+        setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
       }
-    });
+    }
+
+    element = document.getElementById(elementName);
+    endTime = (+new Date()) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+  }
+});
 
 // reset button to set game back to original
 
